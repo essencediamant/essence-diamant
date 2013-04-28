2c5ba09027a3ca8cd23b52aee24311f03c97e473
jepeloqu <jepeloqu@gmail.com>
2013-04-28 16:12:52 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EssenceDiamant::Application.config.secret_key_base = '9142fdda0bf7146229aeee3ef9521ae042d4802189be78a1d0365b0d3e06173b0afe669cf869f9af0f916107c4a02015db1ae9fbd65ba4726459ea83cb8b3456'
