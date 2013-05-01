502430af329fea7d75d95f931a5a5e977f7f0cbd
jepeloqu <jepeloqu@gmail.com>
2013-05-01 12:23:53 +0200

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "Static Pages" do
     	expect(page).to have_content('Essence Diamant France')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
     	visit '/static_pages/home'
-    	expect(page).to have_title("Essence Diamant | Home")
+    	expect(page).to have_title("Essence Diamant")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 end
