783eb759337f1734b4b5cdb3e1a23acebda5f19b
jepeloqu <jepeloqu@gmail.com>
2013-05-01 18:42:41 +0200

    Finish Layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,21 +2,13 @@ require 'spec_helper'
 
 describe "Static Pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Essence Diamant France'" do
-    	visit '/static_pages/home'
-    	expect(page).to have_content('Essence Diamant France')
-    end
-
-    it "should have the base title" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("Essence Diamant")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should_not have_title('| Home')
   end
 end
