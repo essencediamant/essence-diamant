68989a9da6c9f509c553d3b114f1648d527da760
jepeloqu <jepeloqu@gmail.com>
2013-05-16 22:48:13 +0200

    render english and french footers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,13 +10,13 @@ module ApplicationHelper
 	  end
 	end
 
-	# Returns copyright based on page language.
-	def localized_copyright(copyright)
-		french_copyright = '© 2013 APPROCHE DIAMANT (DIAMOND APPROACH), RIDHWAN et le symbole "Hu" sont des marques déposées de la Fondation Ridhwan.'
-		if copyright.empty?
-			french_copyright
-		else
-			copyright
+	# Returns appropriate footer for english or french pages
+	def footer(footer)
+		french_footer = 'layouts/frenchfooter'
+		if footer.empty?
+			french_footer
+		else 
+			footer
 		end
 	end
 end
