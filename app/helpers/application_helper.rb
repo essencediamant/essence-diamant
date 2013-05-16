394685c859101735438baa9f02895ebf674f3306
jepeloqu <jepeloqu@gmail.com>
2013-05-16 14:48:28 +0200

    update copyright to be language dependent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
 	  	"#{base_title} | #{page_title}"
 	  end
 	end
+
+	# Returns copyright based on page language.
+	def localized_copyright(copyright)
+		french_copyright = '© 2013 APPROCHE DIAMANT (DIAMOND APPROACH), RIDHWAN et le symbole "Hu" sont des marques déposées de la Fondation Ridhwan.'
+		if copyright.empty?
+			french_copyright
+		else
+			copyright
+		end
+	end
 end
