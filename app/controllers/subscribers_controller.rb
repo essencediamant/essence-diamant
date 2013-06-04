7c0ac8e56b0748627c4bc659a41a6b114d5c04e4
jepeloqu <jepeloqu@gmail.com>
2013-06-04 22:08:02 +0200

    finish app to mailchimp subscriber pipeline

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -15,16 +15,16 @@ class SubscribersController < ApplicationController
 	def create
 		@subscriber = Subscriber.new(email: params[:subscriber][:email], language: params[:subscriber][:language], active: false)
 		if @subscriber.save
-			Subscriber.delay(queue: "add_subscribers", priority: 10).add_subscriber_to_mailchimp(params[:subscriber][:id])
-			if params[:subscriber][:language] == "french"
+			@subscriber.delay(queue: "add_subscribers", priority: 10).add_subscriber_to_mailchimp
+			if params[:subscriber][:language] == "French"
 				render 'french_subscribe_success'
-		  elsif params[:subscriber][:language] == "english"
+		  elsif params[:subscriber][:language] == "English"
 		 		render 'english_subscribe_success'
 		  end
 		else
-			if params[:subscriber][:language] == "french"
+			if params[:subscriber][:language] == "French"
 				render 'french_subscribe_failure'
-		  elsif params[:subscriber][:language] == "english"
+		  elsif params[:subscriber][:language] == "English"
 		 		render 'english_subscribe_failure'
 		  end
 		end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -4,16 +4,13 @@ class Subscriber < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
 
-  def self.add_subscriber_to_mailchimp(id)
-    find(id).add_subscriber_to_mailchimp
-  end
-
 	def add_subscriber_to_mailchimp
 		gb = Gibbon.new("9a74ef27a5472f7a63e06d6dbe7bba7a-us7")
 
-		gb.list_subscribe({:id => "450a3cc3cd", :email_address => :email, :update_existing => true, 
+		group = { :id => gb.list_interest_groupings(:api_key => "9a74ef27a5472f7a63e06d6dbe7bba7a-us7", :id => "450a3cc3cd")[0]["id"] }
+		gb.list_subscribe({:id => "450a3cc3cd", :email_address => self.email, :update_existing => true, 
 									     :double_optin => false, :send_welcome => false, 
-									     :merge_vars => {'GROUPINGS' => {0 => {'id' => "Language", 'groups' => "French"}}} })
-		update_attribute(active: true)
+									     :merge_vars => {'GROUPINGS' => {0 => {'id' => group[:id], 'groups' => self.language}}} })
+		update_attribute(:active, true)
 	end
 end
