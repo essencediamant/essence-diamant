e872b015fc05b7cb3c717ad9bf43a697660eaee6
jepeloqu <jepeloqu@gmail.com>
2013-08-13 12:58:13 +0100

    update secret_token to be dynamic and random

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-EssenceDiamant::Application.config.secret_key_base = '9142fdda0bf7146229aeee3ef9521ae042d4802189be78a1d0365b0d3e06173b0afe669cf869f9af0f916107c4a02015db1ae9fbd65ba4726459ea83cb8b3456'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
--- script/pull_unsubscribers_from_mailchimp.rb
+++
@@ -1,9 +0,0 @@
-class PullUnsubscribersFromMailchimp < ActiveRecord::Base
-	gb = Gibbon.new("9a74ef27a5472f7a63e06d6dbe7bba7a-us7")
-
-	group = { :id => gb.list_interest_groupings(:api_key => "9a74ef27a5472f7a63e06d6dbe7bba7a-us7", :id => "450a3cc3cd")[0]["id"] }
-	gb.list_subscribe({:id => "450a3cc3cd", :email_address => self.email, :update_existing => true, 
-								     :double_optin => false, :send_welcome => false, 
-								     :merge_vars => {'GROUPINGS' => {0 => {'id' => group[:id], 'groups' => self.language}}} })
-	update_attribute(:active, true)
-end	
